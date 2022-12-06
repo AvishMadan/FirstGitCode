489579f197855b09fe3612c1063cee95f566cb2b
avish <avishtest@gmail.com>
2022-12-06 19:39:38 +0530

    i have added comment tc

---
+++ src/test/java/Testcases/Comments.java
@@ -0,0 +1,18 @@
+package Testcases;
+
+public class Comments {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Comment validate for fb");
+		System.out.println("Comment validate for Insta");
+		
+		System.out.println("Comment validate for whatsapp");
+		
+		
+		
+		
+	}
+}
---
+++ src/test/java/Testcases/Data.java
@@ -0,0 +1,16 @@
+package Testcases;
+
+public class Data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		//test
+	}
+
+}
